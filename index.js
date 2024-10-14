53b36d2f60f750ab51a4b237af6943a21e0730e5
Nika <nikax193@gmail.com>
2024-10-14 23:17:16 +0400

    add  postcontroller

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import Post from "./Post.js";
+import router from "./router.js";
 
 const port = 3000;
 const DB_URL = `mongodb+srv://user123:user123@cluster0.5onyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
@@ -8,12 +8,8 @@ const DB_URL = `mongodb+srv://user123:user123@cluster0.5onyj.mongodb.net/?retryW
 const app = express()
 
 app.use(express.json())
+app.use('/api', router)
 
-app.post('/', async (req, res) => {
-  const {author, title, content, picture} = req.body
-  const post = await Post.create({author, title, content, picture})
-  res.status(200).json('server work with mongodb' )
-})
 
 async function startApp() {
   try {
--- router.js
+++ router.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import PostController from "./PostController.js";
 
 const router  = new Router()
 
-router.post('/posts')
-router.get('/posts')
-router.get('/posts/:id')
-router.put('/posts')
-router.delete('/posts/:id')
+router.post('/posts', PostController.create)
+router.get('/posts', PostController.getAll)
+router.get('/posts/:id', PostController.getOne)
+router.put('/posts', PostController.update)
+router.delete('/posts/:id', PostController.delete)
 
 export default router
