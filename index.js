f7f66cbef5f961a234f84cd5a311be76cfb547fe
Nika <nikax193@gmail.com>
2024-10-14 20:36:52 +0400

    add mongodb, add post file, add router file

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 import express from "express";
+import mongoose from "mongoose";
+import Post from "./Post.js";
+
+const port = 3000;
+const DB_URL = `mongodb+srv://user123:user123@cluster0.5onyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 const app = express()
 
-const port = 3000
-
 app.use(express.json())
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.status(200).json('server worcks' )
+app.post('/', async (req, res) => {
+  const {author, title, content, picture} = req.body
+  const post = await Post.create({author, title, content, picture})
+  res.status(200).json('server work with mongodb' )
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+async function startApp() {
+  try {
+   await mongoose.connect(DB_URL)
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+
+  } catch (e) {
+    console.log(e)
+  }
+  
+}
+
+
+startApp()
+
+
